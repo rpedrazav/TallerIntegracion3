e611094fe2b185f7e6174fddf484990a96cfbe55
Daniel Prado <dprado2024@alu.uct.cl>
2026-09-11 13:47:37 -0300

    Se configuro el IPC
    Se configuro el ipc entre el proceso main  y renderer.

--- globalmart-frontend/src/preload.ts
+++ globalmart-frontend/src/preload.ts
@@ -1,2 +1,8 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { contextBridge, ipcRenderer } from 'electron';
+
+contextBridge.exposeInMainWorld('api', {
+  ping: () => ipcRenderer.invoke('ping'),
+  getToken: () => ipcRenderer.invoke('auth:getToken'),
+  setToken: (token: string) => ipcRenderer.invoke('auth:setToken', token),
+  logout: () => ipcRenderer.invoke('auth:logout'),
+});
---
+++ globalmart-frontend/src/types/global.d.ts
@@ -0,0 +1,12 @@
+﻿export {};
+
+declare global {
+  interface Window {
+    api: {
+      ping: () => Promise<string>;
+      getToken: () => Promise<string | null>;
+      setToken: (token: string) => Promise<void>;
+      logout: () => Promise<void>;
+    };
+  }
+}
